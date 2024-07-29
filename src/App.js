cb8e89070e5c4a796b93b0713566a0c5bf28dba4
FlorianCohenJoly <floriancohenjoy@gmail.com>
2024-07-29 12:29:52 +0200

    feat: Add EndGame component and words.json data
    The commit adds the `EndGame` component and the `words.json` data file. The `EndGame` component displays the winner of the game, and the `words.json` file contains a list of words for the game.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import Calendar from './components/Calendar';
-import './styles/App.scss';
+import React, { useState } from 'react';
+import Setup from './components/Setup';
+import DrawCard from './components/DrawCard';
+import Game from './components/Game';
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [roles, setRoles] = useState([]);
+  const [wordAssignments, setWordAssignments] = useState({});
+  const [phase, setPhase] = useState('setup');
+
+  const startGame = (players, roles, wordAssignments) => {
+    setPlayers(players);
+    setRoles(roles);
+    setWordAssignments(wordAssignments);
+    setPhase('draw');
+  };
+
+  const startGamePhase = () => {
+    setPhase('game');
+  };
+
   return (
-    <div className="App">
-      <Calendar />
+    <div>
+      {phase === 'setup' && <Setup startGame={startGame} />}
+      {phase === 'draw' && <DrawCard players={players} wordAssignments={wordAssignments} handleNext={startGamePhase} />}
+      {phase === 'game' && <Game players={players} roles={roles} wordAssignments={wordAssignments} />}
     </div>
   );
 }
